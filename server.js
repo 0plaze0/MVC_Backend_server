8e9194049d344e1631cd5590ea06268ce592a4a4
plaze <banikakash131@gamil.com>
2023-06-02 12:22:05 +0530

    middleware logger.js added

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+const { logger } = require("./middleware/logger");
+
+//middleware
+app.use(logger);
+
 app.listen(3500, () => {
   console.log(`Server is running in ${PORT}`);
 });
