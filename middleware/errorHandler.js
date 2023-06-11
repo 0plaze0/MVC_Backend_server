b2f20b771e80e2877dab1040d9eb47655e14c721
plaze <banikakash131@gamil.com>
2023-06-11 13:08:50 +0530

    Error Handler middle added

---
+++ middleware/errorHandler.js
@@ -0,0 +1,9 @@
+const { logEvents } = require("./logger.js");
+
+const errorHandler = (err, req, res, next) => {
+  logEvents(`${err.name}\t${err.message}`, "errLog.txt");
+  console.error(err.stack);
+  res.status(500).send(err.message);
+};
+
+module.exports = { errorHandler };
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const corsOption = require("./config/corsOptions");
+
+const { errorHandler } = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 const { logger } = require("./middleware/logger");
@@ -14,10 +16,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/subdir", express.static(path.join(__dirname, "public")));
 
+//serve file
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+//404 page not found
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 not found" });
+  } else {
+    res.type("text").send("404 not found");
+  }
+});
+
+//error handling
+app.use(errorHandler);
+//listen in port
 app.listen(3500, () => {
   console.log(`Server is running in ${PORT}`);
 });
